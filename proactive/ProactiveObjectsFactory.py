3b363970643b3f00a8b852d9d6e074210581cb50
marcocast <marcocast@gmail.com>
2018-04-05 17:21:25 +0200

    Separating the creation of proactive objects

---
+++ proactive/ProactiveObjectsFactory.py
@@ -0,0 +1,27 @@
+import os
+import cloudpickle
+import codecs
+
+from .ScriptLanguage import *
+
+
+class ProactiveObjectsFactory:
+
+  runtime_gateway = None
+
+  def __init__(self, runtime_gateway):
+    self.runtime_gateway = runtime_gateway
+
+  def create_simple_script(self, script_code, script_language):
+    return self.runtime_gateway.jvm.org.ow2.proactive.scripting.SimpleScript(script_code, script_language)
+
+  def create_task_script(self, simple_script):
+    return self.runtime_gateway.jvm.org.ow2.proactive.scripting.TaskScript(simple_script)
+
+  def create_script_task(self):
+    return self.runtime_gateway.jvm.org.ow2.proactive.scheduler.common.task.ScriptTask()
+
+  def create_job(self):
+    return self.runtime_gateway.jvm.org.ow2.proactive.scheduler.common.job.TaskFlowJob()
+
+  
--- proactive/SerialisationHelper.py
+++ proactive/SerialisationHelper.py
@@ -3,6 +3,8 @@ import cloudpickle
 import codecs
 
 from .ScriptLanguage import *
+from .ProactiveObjectsFactory import *
+
 
 
 class SerialisationHelper:
@@ -12,23 +14,13 @@ class SerialisationHelper:
   """
   runtime_gateway = None
   script_language = None
+  proactive_object_factory = None
 
   def __init__(self, runtime_gateway):
-    self.runtime_gateway = runtime_gateway
+    self.proactive_object_factory = ProactiveObjectsFactory(runtime_gateway)
     self.script_language = ScriptLanguage()
 
-  def create_simple_script(self, script_code, script_language):
-    return self.runtime_gateway.jvm.org.ow2.proactive.scripting.SimpleScript(script_code, script_language)
-
-  def create_task_script(self, simple_script):
-    return self.runtime_gateway.jvm.org.ow2.proactive.scripting.TaskScript(simple_script)
-
-  def create_script_task(self):
-    return self.runtime_gateway.jvm.org.ow2.proactive.scheduler.common.task.ScriptTask()
-
-  def create_job(self):
-    return self.runtime_gateway.jvm.org.ow2.proactive.scheduler.common.job.TaskFlowJob()
-
+ 
   def create_python_task_from_file(self, file_python, script_params={}):
     script_python = ''
     if os.path.exists(file_python):
@@ -37,9 +29,9 @@ class SerialisationHelper:
     return self.create_python_task_from_script(script_python, script_params)
 
   def create_python_task_from_script(self, script_python, script_params={}):
-    python_script = self.create_simple_script(script_python, self.script_language.python())
-    task_script = self.create_task_script(python_script)
-    script_task = self.create_script_task()
+    python_script = self.proactive_object_factory.create_simple_script(script_python, self.script_language.python())
+    task_script = self.proactive_object_factory.create_task_script(python_script)
+    script_task = self.proactive_object_factory.create_script_task()
 
     if 'TASK_NAME' in script_params:
       script_task.setName(script_params["TASK_NAME"])
@@ -50,7 +42,7 @@ class SerialisationHelper:
     if 'PYTHON_COMMAND' in script_params:
       script_task.addGenericInformation("PYTHON_COMMAND", script_params["PYTHON_COMMAND"])
 
-    job = self.create_job()
+    job = self.proactive_object_factory.create_job()
     if 'JOB_NAME' in script_params:
       job.setName(script_params["JOB_NAME"])
     else:
