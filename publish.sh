978662120983a24c6425473a56cc8f1c14d3dbef
Andrews Sobral <andrewssobral@gmail.com>
2020-05-21 22:18:23 +0200

    Forced python3 and pip3

--- publish.sh
+++ publish.sh
@@ -3,12 +3,21 @@ command -v source >/dev/null 2>&1 || {
   echo "I require source but it's not installed.  Aborting." >&2; exit 1;
 }
 
+echo "HOME: $HOME"
+echo "PWD: $PWD"
+
+which python3
+which pip3
+python3 -V
+pip3 -V
+
+pip3 list
 virtualenv -p python3 env
 source env/bin/activate
 
-pip list
-pip install twine
-pip list
+pip3 list
+pip3 install twine
+pip3 list
 
 # find / -name .pypirc
 PYPIRC_FILE=/home/activeeon/.pypirc
