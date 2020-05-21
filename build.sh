1a9c82666c400d7ec24c986d3f68ac900f66118d
Andrews Sobral <andrewssobral@gmail.com>
2020-05-21 22:18:23 +0200

    Forced python3 and pip3
    (cherry picked from commit 978662120983a24c6425473a56cc8f1c14d3dbef)

--- build.sh
+++ build.sh
@@ -6,22 +6,27 @@ command -v source >/dev/null 2>&1 || {
 echo "HOME: $HOME"
 echo "PWD: $PWD"
 
-pip install virtualenv
-#which virtualenv
+which python3
+which pip3
+python3 -V
+pip3 -V
+
+pip3 list
+pip3 install virtualenv
+pip3 install -U pip
+pip3 list
 
 virtualenv -p python3 env
 #virtualenv -p python2 env
 source env/bin/activate
 
-pip list
-pip install -U pip
-pip install jprops
-pip install py4j==0.10.8.1
-pip install pytest-html requests cloudpickle
-
+pip3 list
+pip3 install jprops
+pip3 install py4j==0.10.8.1
+pip3 install pytest-html requests cloudpickle
 python setup.py sdist --formats=zip
-pip install dist/proactive*.zip
-pip list
+pip3 install dist/proactive*.zip
+pip3 list
 
 if [ -z "$1" ]
   then
