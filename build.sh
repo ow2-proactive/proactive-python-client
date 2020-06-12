ee747737c7e52bdb591eb6cd9f9601260b8e4c8c
Andrews Sobral <andrewssobral@gmail.com>
2020-06-12 16:57:44 +0200

    Updated publish.sh and build.sh. Forced python3 and pip3.

--- build.sh
+++ build.sh
@@ -3,23 +3,30 @@ command -v source >/dev/null 2>&1 || {
   echo "I require source but it's not installed.  Aborting." >&2; exit 1;
 }
 
-#pip list
+echo "HOME: $HOME"
+echo "PWD: $PWD"
 
-#pip install virtualenv
-#which virtualenv
+which python3
+which pip3
+python3 -V
+pip3 -V
+
+pip3 list
+pip3 install -U pip
+pip3 install virtualenv
+pip3 list
 
 virtualenv -p python3 env
 #virtualenv -p python2 env
 source env/bin/activate
 
-pip install jprops
-
+pip3 list
+pip3 install jprops
+pip3 install py4j==0.10.8.1
+pip3 install pytest-html requests cloudpickle
 python setup.py sdist --formats=zip
-pip install dist/proactive*.zip
-
-pip install pytest-html requests py4j cloudpickle
-
-#pip list
+pip3 install dist/proactive*.zip
+pip3 list
 
 if [ -z "$1" ]
   then
--- publish.sh
+++ publish.sh
@@ -3,14 +3,29 @@ command -v source >/dev/null 2>&1 || {
   echo "I require source but it's not installed.  Aborting." >&2; exit 1;
 }
 
+echo "HOME: $HOME"
+echo "PWD: $PWD"
+
+which python3
+which pip3
+python3 -V
+pip3 -V
+
+pip3 list
 virtualenv -p python3 env
 source env/bin/activate
 
-pip install twine
+pip3 list
+pip3 install twine
+pip3 list
 
-if [[ $JENKINS_JNLP_URL ]]
-then
-   twine upload -r pypi dist/* --config-file /home/activeeon/.pypirc
+# find / -name .pypirc
+PYPIRC_FILE=/home/activeeon/.pypirc
+if [ -f "$PYPIRC_FILE" ]; then
+  echo "$PYPIRC_FILE exist"
+  twine upload -r pypi dist/* --config-file $PYPIRC_FILE
+  echo "Done"
 else
-   twine upload -r pypi dist/*
+  echo "$PYPIRC_FILE does not exist"
+  echo "The upload to pypi was aborted!"
 fi
