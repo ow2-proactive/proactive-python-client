bee2903e0ef700d47751fa2c9bc7dc4fc0da88dc
Mohamed Khalil LABIDI <47785603+mklkun@users.noreply.github.com>
2020-09-23 10:41:04 +0200

    Add basic configuration for automated documentation generation (#30)
    * Add basic configuration for automated documentation genneration
    
    * Add clean in build.gradle
    
    (cherry picked from commit a5fc695c9b72e4a6deeab998abfca4a4e9e54ceb)

---
+++ docs/conf.py
@@ -0,0 +1,72 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+import datetime
+sys.path.insert(0, os.path.abspath('..'))
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'proactive-python-client'
+copyright = '2020, Activeeon'
+author = 'Activeeon'
+
+# The version info for the project you're documenting, acts as replacement for
+# |version| and |release|, also used in various other places throughout the
+# built documents.
+#
+# The short X.Y version.
+now = datetime.datetime.now()
+gradle_properties={}
+with open('../gradle.properties') as fp:
+    for line in fp:
+        if '=' in line:
+            name, value = line.replace('\n','').split('=', 1)
+            if "SNAPSHOT" in value:
+                dev_version = "." + now.strftime("%y%m%d%H%M") + "dev"
+                # dev_version = "." + now.strftime("%y%m%d%H%M")
+                value = value.replace("-SNAPSHOT", dev_version)
+            gradle_properties[name] = value
+version = gradle_properties["version"]
+# The full version, including alpha/beta/rc tags.
+#release = u'text'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = ['sphinx.ext.autodoc']
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'sphinx_rtd_theme'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
--- proactive/ProactiveGateway.py
+++ proactive/ProactiveGateway.py
@@ -22,10 +22,19 @@ from .model.ProactiveJob import *
 class ProActiveGateway:
     """
     Simple client for the ProActive scheduler REST API
-    See also https://try.activeeon.com/rest/doc/jaxrsdocs/overview-summary.html
+    See also https://try.activeeon.com/doc/rest/
     """
 
     def __init__(self, base_url, debug=False, javaopts=[], log4j_props_file=None, log4py_props_file=None):
+        """
+        Create a Proactive Gateway
+
+        :param base_url: The Proactive server base URL
+        :param debug: If set True, the gateway will be created with the debug mode
+        :param javaopts: Some additional java options
+        :param log4j_props_file: The log4j properties file path
+        :param log4py_props_file: The log4py properties file path
+        """
         self.root_dir = os.path.dirname(os.path.abspath(__file__))
         self.current_path = self.root_dir + "/java/lib/*"
         self.base_url = base_url
@@ -77,6 +86,14 @@ class ProActiveGateway:
         self.proactive_scheduler_client = self.proactive_factory.create_smart_proxy()
 
     def connect(self, username=None, password=None, credentials_path=None, insecure=True):
+        """
+        Connect to a Proative server
+
+        :param username: A valid username
+        :param password: A valid password
+        :param credentials_path: A credentials file path
+        :param insecure: If set True, the gateway will connect in insecure mode
+        """
         credentials_file = None
         if credentials_path is not None:
             credentials_file = self.runtime_gateway.jvm.java.io.File(credentials_path)
@@ -95,55 +112,114 @@ class ProActiveGateway:
         self.logger.debug('Connected on ' + self.base_url)
 
     def isConnected(self):
+        """
+        Verify if the gateway is connected to the ProActive server
+
+        :return: True or False
+        """
         return self.proactive_scheduler_client.isConnected()
 
     def disconnect(self):
+        """
+        Disconnect the gateway from the ProActive server
+        """
         self.logger.debug('Disconnecting from the ProActive server')
         self.proactive_scheduler_client.disconnect()
         self.logger.debug('Disconnected.')
 
     def reconnect(self):
+        """
+        Reconnect the gateway to the ProActive server
+        """
         self.logger.debug('Reconnecting to the ProActive server')
         self.proactive_scheduler_client.reconnect()
         self.logger.debug('Reconnected')
 
     def terminate(self):
+        """
+        Terminate the connection
+        """
         self.proactive_scheduler_client.terminate()
 
     def submitWorkflowFromCatalog(self, bucket_name, workflow_name, workflow_variables={}):
+        """
+        Submit a job from the catalog to the scheduler
 
+        :param bucket_name: The bucket in which the workflow is saved
+        :param workflow_name: The workflow name
+        :param workflow_variables: The workflow input variables
+        :return: The submitted job id
+        """
         workflow_variables_java_map = MapConverter().convert(workflow_variables, self.runtime_gateway._gateway_client)
         self.logger.debug('Submitting from catalog the job \'' + bucket_name + '/' + workflow_name + '\'')
         return self.proactive_scheduler_client.submitFromCatalog(self.base_url + "/catalog", bucket_name, workflow_name,
                                                                  workflow_variables_java_map).longValue()
 
     def submitWorkflowFromFile(self, workflow_xml_file_path, workflow_variables={}):
+        """
+        Submit a job from an xml file to the scheduler
+
+        :param workflow_xml_file_path: The workflow xml file path
+        :param workflow_variables: The workflow input variables
+        :return: The submitted job id
+        """
         workflow_variables_java_map = MapConverter().convert(workflow_variables, self.runtime_gateway._gateway_client)
         self.logger.debug('Submitting from file the job \'' + workflow_xml_file_path + '\'')
         return self.proactive_scheduler_client.submit(self.runtime_gateway.jvm.java.io.File(workflow_xml_file_path),
                                                       workflow_variables_java_map).longValue()
 
     def submitWorkflowFromURL(self, workflow_url_spec, workflow_variables={}):
+        """
+        Submit a job from an url to the scheduler
+
+        :param workflow_url_spec: The workflow url
+        :param workflow_variables: The workflow input variables
+        :return: The submitted job id
+        """
         workflow_variables_java_map = MapConverter().convert(workflow_variables, self.runtime_gateway._gateway_client)
         self.logger.debug('Submitting from URL the job \'' + workflow_url_spec + '\'')
         return self.proactive_scheduler_client.submit(self.runtime_gateway.jvm.java.net.URL(workflow_url_spec),
                                                       workflow_variables_java_map).longValue()
 
     def createTask(self, language=None):
+        """
+        Create a workflow task
+
+        :param language: The script language
+        :return: A ProactiveTask object
+        """
         self.logger.info('Creating a task')
         return ProactiveTask(language) if self.proactive_script_language.is_language_supported(language) else None
 
     def createPythonTask(self):
+        """
+        Create a workflow Python task
+
+        :return: A Python ProactiveTask object
+        """
         self.logger.info('Creating a Python task')
         return ProactivePythonTask()
 
     def createFlowScript(self, script_language=None):
+        """
+        Create a flow script
+
+        :param script_language: The script language
+        :return: A ProactiveFlowScript object
+        """
         if script_language is None:
             script_language = self.proactive_script_language.javascript()
         self.logger.info('Creating a flow script')
         return ProactiveFlowScript(script_language)
 
     def createReplicateFlowScript(self, script_implementation, script_language="javascript"):
+        """
+        Create a replicate flow script
+
+        :param script_implementation: The script implementation
+        :param script_language: The script language
+        :return: A replicate ProactiveFlowScript object
+        """
         self.logger.info('Creating a Replicate flow script')
         flow_script = ProactiveFlowScript(script_language)
         flow_script.setActionType(self.proactive_flow_action_type.replicate())
@@ -151,6 +227,14 @@ class ProActiveGateway:
         return flow_script
 
     def createLoopFlowScript(self, script_implementation, target, script_language="javascript"):
+        """
+        Create a loop flow script
+
+        :param script_implementation: The script implementation
+        :param target: The loop target
+        :param script_language: The script language
+        :return: A loop ProactiveFlowScript object
+        """
         self.logger.info('Creating a Loop flow script')
         flow_script = ProactiveFlowScript(script_language)
         flow_script.setActionType(self.proactive_flow_action_type.loop())
@@ -160,6 +244,16 @@ class ProActiveGateway:
 
     def createBranchFlowScript(self, script_implementation, target_if, target_else, target_continuation,
                                script_language="javascript"):
+        """
+        Create a branch flow script
+
+        :param script_implementation: The script implementation
+        :param target_if: The if target task
+        :param target_else: The else target task
+        :param target_continuation: The continuationn target task
+        :param script_language: The script language
+        :return: A branch ProactiveFlowScript object
+        """
         self.logger.info('Creating a Branch flow script')
         flow_script = ProactiveFlowScript(script_language)
         flow_script.setActionType(self.proactive_flow_action_type.branch())
@@ -170,30 +264,75 @@ class ProActiveGateway:
         return flow_script
 
     def getProactiveFlowBlockType(self):
+        """
+        Get the Proactive flow block
+
+        :return: The ProactiveFlowBlock object
+        """
         return self.proactive_flow_block
 
     def createPreScript(self, language=None):
+        """
+        Create a Proactive pre-script
+
+        :param language: The script language
+        :return: A ProactivePreScript object
+        """
         self.logger.info('Creating a pre script')
         return ProactivePreScript(language) if self.proactive_script_language.is_language_supported(language) else None
 
     def createPostScript(self, language=None):
+        """
+        Create a Proactive post-script
+
+        :param language: The script language
+        :return: A ProactivePostScript object
+        """
         self.logger.info('Creating a post script')
         return ProactivePostScript(language) if self.proactive_script_language.is_language_supported(language) else None
 
     def createJob(self):
+        """
+        Create a Proactive Job
+
+        :return: A ProactiveJob object
+        """
         self.logger.info('Creating a job')
         return ProactiveJob()
 
     def buildJob(self, job_model, debug=False):
+        """
+        Build the Proactive job to be submitted to the scheduler
+
+        :param job_model: A valid job model
+        :param debug: If set True, the submitted job will be printed for a debugging purpose
+        :return: A Proactive job ready to be submitted
+        """
         self.logger.info('Building the job' + job_model.getJobName())
         return ProactiveJobBuilder(self.proactive_factory, job_model, self.debug, self.log4py_props_file).create().display(debug).getProactiveJob()
 
     def submitJob(self, job_model, debug=False):
+        """
+        Submit a job to the Proactive Scheduler
+
+        :param job_model: A valid job model
+        :param debug: If set True, the submitted job will be printed for a debugging purpose
+        :return: The submitted job ID
+        """
         proactive_job = self.buildJob(job_model, debug)
         self.logger.info('Submitting the job' + job_model.getJobName())
         return self.proactive_scheduler_client.submit(proactive_job).longValue()
 
     def submitJobWithInputsAndOutputsPaths(self, job_model, input_folder_path='.', output_folder_path='.', debug=False):
+        """
+        Submit a job to the Proactive Scheduler with input and output paths
+
+        :param job_model: A valid job model
+        :param input_folder_path: Path to the directory containing input files
+        :param output_folder_path: Path to the local directory which will contain output files
+        :param debug: If set True, the submitted job will be printed for a debugging purpose
+        :return: The submitted job ID
+        """
         proactive_job = self.buildJob(job_model, debug)
         self.logger.info('Submitting the job' + job_model.getJobName())
         return self.proactive_scheduler_client.submit(
@@ -205,45 +344,111 @@ class ProActiveGateway:
         ).longValue()
 
     def createForkEnvironment(self, language=None):
+        """
+        Create a Proactive fork environment
+
+        :param language: The script language
+        :return: A ProactiveForkEnv object
+        """
         self.logger.info('Creating a fork environment')
         return ProactiveForkEnv(language) if self.proactive_script_language.is_language_supported(language) else None
 
     def createDefaultForkEnvironment(self):
+        """
+        Create a default fork environment
+
+        :return: A default ProactiveForkEnv object
+        """
         self.logger.info('Creating a default fork environment')
         return ProactiveForkEnv(self.proactive_script_language.jython())
 
     def createPythonForkEnvironment(self):
+        """
+        Create a Python fork environment
+
+        :return: A Python ProactiveForkEnv object
+        """
         self.logger.info('Creating a Python fork environment')
         return ProactiveForkEnv(self.proactive_script_language.python())
 
     def createSelectionScript(self, language=None):
+        """
+        Create a Proactive selection script
+
+        :param language: The script language
+        :return: A ProactiveSelectionScript object
+        """
         self.logger.info('Creating a selection script')
         return ProactiveSelectionScript(language) if self.proactive_script_language.is_language_supported(language) else None
 
     def createDefaultSelectionScript(self):
+        """
+        Create a default selection script
+
+        :return: A default ProactiveSelectionScript object
+        """
         self.logger.info('Creating a default selection script')
         return ProactiveSelectionScript(self.proactive_script_language.jython())
 
     def createPythonSelectionScript(self):
+        """
+        Create a Python selection script
+
+        :return: A Python ProactiveSelectionScript object
+        """
         self.logger.info('Creating a Python selection script')
         return ProactiveSelectionScript(self.proactive_script_language.python())
 
     def getProactiveClient(self):
+        """
+        Get the Proactive gateway
+
+        :return: A smart proxy object
+        """
         return self.proactive_scheduler_client
 
     def getProactiveScriptLanguage(self):
+        """
+        Get Proactive script languages
+
+        :return: The ProactiveScriptLanguage object
+        """
         return self.proactive_script_language
 
     def getJobState(self, job_id):
+        """
+        Get the job state
+
+        :param job_id: A valid job ID
+        :return: The job state
+        """
         return self.proactive_scheduler_client.getJobState(job_id).getName()
 
     def isJobFinished(self, job_id):
+        """
+        Verify if a job is finished
+
+        :param job_id: A valid job ID
+        :return: True or False
+        """
         return self.proactive_scheduler_client.isJobFinished(job_id)
 
     def getJobInfo(self, job_id):
+        """
+        Get the job info
+
+        :param job_id: A valid job ID
+        :return: The job info
+        """
         return self.proactive_scheduler_client.getJobInfo(str(job_id))
 
     def getAllJobs(self, max_number_of_jobs=1000):
+        """
+        Get all jobs from the ProActive scheduler
+
+        :param max_number_of_jobs: The maximal number of retrieved jobs
+        :return: A list of jobs
+        """
         job_filter_criteria = self.runtime_gateway.jvm.org.ow2.proactive.scheduler.common.JobFilterCriteria(False, False,
                                                                                                             True, False)
         jobs_page = self.proactive_scheduler_client.getJobs(0, max_number_of_jobs, job_filter_criteria, None)
@@ -254,6 +459,13 @@ class ProActiveGateway:
         return value.decode('ascii')
 
     def getJobResult(self, job_id, timeout=60000):
+        """
+        Get the job result
+
+        :param job_id: A valid job ID
+        :param timeout: A timeout in milliseconds
+        :return: The job results
+        """
         self.logger.debug('Getting job\'s results')
         job_result = self.proactive_scheduler_client.waitForJob(str(job_id), timeout)
         all_results = []
@@ -266,6 +478,14 @@ class ProActiveGateway:
         return os.linesep.join(v for v in all_results)
 
     def getTaskResult(self, job_id, task_name, timeout=60000):
+        """
+        Get the task result
+
+        :param job_id: A valid job ID
+        :param task_name: A valid task name
+        :param timeout: A timeout in milliseconds
+        :return: The task results
+        """
         self.logger.debug('Getting results of the task \'' + task_name + '\'')
         task_result = self.proactive_scheduler_client.waitForTask(str(job_id), task_name, timeout)
         self.logger.debug('Formatting results')
@@ -274,6 +494,13 @@ class ProActiveGateway:
         return task_result.getValue()
 
     def printJobOutput(self, job_id, timeout=60000):
+        """
+        Get the job output
+
+        :param job_id: A valid job ID
+        :param timeout: A timeout in milliseconds
+        :return: The job outputs
+        """
         self.logger.debug('Getting job\'s outputs')
         job_result = self.proactive_scheduler_client.waitForJob(str(job_id), timeout)
         all_outputs = []
@@ -283,17 +510,39 @@ class ProActiveGateway:
         return os.linesep.join(v for v in all_outputs)
 
     def printTaskOutput(self, job_id, task_name, timeout=60000):
+        """
+        Get the task outputs
+
+        :param job_id: A valid job ID
+        :param task_name: A valid task name
+        :param timeout: A timeout in milliseconds
+        :return: The task outputs
+        """
         self.logger.debug('Getting the task \'' + task_name + '\'\'s outputs')
         task_output = self.proactive_scheduler_client.waitForTask(str(job_id), task_name, timeout).getOutput()
         return task_output.getStdoutLogs().strip(' \n')
 
     def exportJob2XML(self, job_model, debug=False):
+        """
+        Export the job to an XML file
+
+        :param job_model: A valid job model
+        :param debug: If set True, the created job will be printed for a debugging purpose
+        :return: The created job as an XML script
+        """
         proactive_job = self.buildJob(job_model, debug)
         self.logger.info('Transforming the job \'' + job_model.getJobName() + '\' to an XML string')
         Job2XMLTransformer = self.proactive_factory.create_job2xml_transformer()
         return Job2XMLTransformer.jobToxmlString(proactive_job)
 
     def saveJob2XML(self, job_model, xml_file_path, debug=False):
+        """
+        Save a job to an external XML file
+
+        :param job_model: A valid job model
+        :param xml_file_path: A valid XML file path
+        :param debug: If set True, the created job will be printed for a debugging purpose
+        """
         self.logger.info('Saving the job \'' + job_model.getJobName() + '\' to the XML file \'' + xml_file_path + '\'')
         job_xml_data = self.exportJob2XML(job_model, debug)
         with open(xml_file_path, "w") as text_file:
