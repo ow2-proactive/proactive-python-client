ef6b9598749538ab5f29ed0e69ce58d62d1bca1e
Andrews Cordolino Sobral <andrewssobral@gmail.com>
2024-03-10 13:36:25 +0100

    Added __version__ to the proactive module

--- proactive/__init__.py
+++ proactive/__init__.py
@@ -10,3 +10,9 @@ from .model.ProactiveSelectionScript import *
 from .model.ProactiveScriptLanguage import *
 from .model.ProactiveTask import *
 from .model.ProactiveJob import *
+
+import os
+version_file = os.path.join(os.path.dirname(__file__), '..', 'VERSION')
+
+with open(version_file) as vf:
+    __version__ = vf.read().strip()
